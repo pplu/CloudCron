a27bbf3e9a455cf864b1a576e823d76324dc9311
Eduard Badillo <eduard.badillo@capside.com>
2017-02-13 14:46:26 +0000

    Add stack_version

--- lib/CloudCron/AWS/Queue.pm
+++ lib/CloudCron/AWS/Queue.pm
@@ -35,6 +35,8 @@ package CloudCron::AWS::Queue {
     default => sub { CloudCronSQSQueueArgs->new_with_options()  },
   );
 
+  stack_version 1;
+
   resource CloudCronDeadLetterQueue => 'AWS::SQS::Queue', {
     MessageRetentionPeriod => 1209600,  # 14 days (it's the maximum value)          
   };
