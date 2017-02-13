d360448528e3c8ea522f57258987367555b5a33a
Eduard Badillo <eduard.badillo@capside.com>
2017-02-13 12:11:06 +0000

    Add system call to clouddeploy

--- lib/CloudCronCLI/DeployQueue.pm
+++ lib/CloudCronCLI/DeployQueue.pm
@@ -5,9 +5,43 @@ command_short_description q(This is the short description for deployqueue);
 command_long_description q(This is the long description for deployqueue); 
 command_usage q(cloudcron deployqueue);
 
+option name => (
+  is => 'ro',
+  isa => 'Str',
+  required => 1,
+  documentation => 'Queue name to be deployed.',
+);
+
+option region => (
+  is => 'ro',
+  isa => 'Str',
+  required => 1,
+  documentation => 'Region from AWS to deploy Queue to.'
+);
+
 sub run {
   my ($self) = @_;
-}
+
+  chdir ('./lib');
+
+  my @args = (
+    'clouddeploy',
+    'deploy',
+    'CloudCron/AWS/Queue.pm',
+    '--',
+    '--name',
+    $self->name,
+    '--region',
+    $self->region,
+  );
+
+  eval {
+    system(@args);
+  };
+
+  warn $@ if $@;
+
+};
 
 
 1;
