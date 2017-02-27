e5850af6427d95fdfcc519c368b1c68dbcf8fe0c
Jose Luis Martinez <jlmartinez@capside.com>
2017-02-27 08:41:23 +0000

    Cleanup in CloudCron::AWS::CloudWatch
    CustomParams -> didn't need to redefine name and region (inherited from CommonArgs)
    get_deployer -> not needed. The inherited version is OK
    
    added an output to signal that this is a cloudwatch deployment

--- lib/CloudCron/AWS/CloudWatch.pm
+++ lib/CloudCron/AWS/CloudWatch.pm
@@ -1,39 +1,20 @@
 package CloudCron::AWS::CloudWatch::CustomParams {
-    use Moose;
-    extends 'CCfnX::CommonArgs';
-
-
-    has name => (
-        is => 'ro',
-        isa => 'Str',
-        required => 0,
-        default => sub { 'CloudCronWatchEventsRules' },
-        documentation => 'Name of the CWE');
-
-    has region => (
-        is => 'ro',
-        isa => 'Str',
-        required => 1,
-        documentation => 'AWS region');
+  use Moose;
+  extends 'CCfnX::CommonArgs';
 };
 
 package CloudCron::AWS::CloudWatch {
-    use Moose;
-    extends 'CCfn';
+  use Moose;
+  extends 'CCfn';
+  use CCfnX::Shortcuts;
 
-    has params => (
-        is => 'ro',
-        isa => 'CloudCron::AWS::CloudWatch::CustomParams',
-        required => 1,
-    );
+  has 'params' => (
+    is => 'ro',
+    isa => 'CloudCron::AWS::CloudWatch::CustomParams',
+    required => 1,
+  );
 
-    sub get_deployer {
-        my $self = shift;
-        my $deployer = CCfnX::Deployment->new_with_roles(
-            { origin => $self },
-            'CCfnX::CloudFormationDeployer');#, 'CCvnX::PersistentDeployment');
-        return $deployer;
-    }
-};
+  output CloudCronVersion => '1';
+}
 
 1;
